f4f7f6d88b108d28976f33d87be4d960b85e0035
GAS-ART <temoha1386@gmail.com>
2025-08-28 22:02:34 +0300

    Fixed contacts

--- resources/lang/ru/contacts.php
+++ resources/lang/ru/contacts.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-        'phone1' => '+373 (060) 254-815',
-        'phone1_href' => '+373060254815',
-        'phone2' => '+373 (078) 785-925',
-        'phone2_href' => '+373078785925',
+        'phone1' => '+373 60 254-815',
+        'phone1_href' => '+37360254815',
+        'phone2' => '+373 78 785-925',
+        'phone2_href' => '+37378785925',
         'viber' => 'viber://chat?number=%2B37378785925',
         'whatsup' => 'https://wa.me/37378785925', 
         'tg' => 'tg://resolve?domain=brewshaine',
